71be684cafe2e51f9dfa56e6e5f7c127bb61db75
yosep <yosepalexsander9@gmail.com>
2021-11-21 03:24:58 +0700

    remove manual env load

--- main.go
+++ main.go
@@ -12,19 +12,13 @@ import (
 	"github.com/NYTimes/gziphandler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/rs/cors"
 	"github.com/yosepalexsander/waysbucks-api/db"
 	"github.com/yosepalexsander/waysbucks-api/interactor"
 	"github.com/yosepalexsander/waysbucks-api/router"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
 func main() {
 	var dbStore db.DBStore
 	db.Connect(&dbStore)
